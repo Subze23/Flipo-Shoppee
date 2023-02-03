9c2c10cf731b8a8e65d13d4fc59787b0c41fedaf
Subze23 <subzesubhashsanth@gmail.com>
2023-02-03 06:37:06 +0530

    final connections

--- MyApplication3/app/src/main/java/com/example/myapplication/Item1.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/Item1.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Item1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item1)
+        var buttoneasy: Button?=null
+        buttoneasy=findViewById<Button>(R.id.button1)
+        buttoneasy?.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Order Placed", Toast.LENGTH_SHORT).show()
+        })
     }
 }
--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity4.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -1,15 +1,24 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity4 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+        var buttoneasy: ImageView?=null
+        buttoneasy=findViewById<ImageView>(R.id.sample)
+        buttoneasy?.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, Item1::class.java)
+            startActivity(intent)
+        })
 
 
     }
